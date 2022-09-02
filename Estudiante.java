e08c6da7e6b88223f8ad26bc9dce1ed45e6a3582
SonSey10 <irvingfanlin10@gmail.com>
2022-09-02 15:40:19 -0600

    Ultima Carga

---
+++ Estudiante.java
@@ -0,0 +1,258 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.primeratareajava;
+
+/**
+ *
+ * @author Irving Fan
+ */
+/*Se ultiliza un metodo de encapsulacion para la clase estudiantes*/
+/*Se utitilizo un metodo de herencia en estudiantes hacia la interfaz*/
+
+
+public class Estudiante {
+    private String Carne;
+    private String Nombre;
+    private String Apellido;
+    private String Telefono;
+    private String Nickname;
+    private String Tipo_estudiante;
+    private int nota_promedio_examenes;
+    private int nota_promedio_quices;
+    private int nota_promedio_tareas;
+    private int nota_Proyecto1;
+    private int nota_Proyecto2;
+    private int nota_Proyecto3;
+    private int ID;
+    private int nota_promedio_examen_quiz_tarea;
+    private int nota_promedio_proyectos;
+    private int nota_final;
+            
+    
+    /**
+     * @return the Carne
+     */
+    public String getCarne() {
+        return Carne;
+    }
+
+    /**
+     * @param Carne the Carne to set
+     */
+    public void setCarne(String Carne) {
+        this.Carne = Carne;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Apellido
+     */
+    public String getApellido() {
+        return Apellido;
+    }
+
+    /**
+     * @param Apellido the Apellido to set
+     */
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    /**
+     * @return the Telefono
+     */
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    /**
+     * @param Telefono the Telefono to set
+     */
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    /**
+     * @return the Nickname
+     */
+    public String getNickname() {
+        return Nickname;
+    }
+
+    /**
+     * @param Nickname the Nickname to set
+     */
+    public void setNickname(String Nickname) {
+        this.Nickname = Nickname;
+    }
+
+    /**
+     * @return the Tipo_estudiante
+     */
+    public String getTipo_estudiante() {
+        return Tipo_estudiante;
+    }
+
+    /**
+     * @param Tipo_estudiante the Tipo_estudiante to set
+     */
+    public void setTipo_estudiante(String Tipo_estudiante) {
+        this.Tipo_estudiante = Tipo_estudiante;
+    }
+
+    /**
+     * @return the nota_promedio_examenes
+     */
+    public int getNota_promedio_examenes() {
+        return nota_promedio_examenes;
+    }
+
+    /**
+     * @param nota_promedio_examenes the nota_promedio_examenes to set
+     */
+    public void setNota_promedio_examenes(int nota_promedio_examenes) {
+        this.nota_promedio_examenes = nota_promedio_examenes;
+    }
+
+    /**
+     * @return the nota_promedio_quices
+     */
+    public int getNota_promedio_quices() {
+        return nota_promedio_quices;
+    }
+
+    /**
+     * @param nota_promedio_quices the nota_promedio_quices to set
+     */
+    public void setNota_promedio_quices(int nota_promedio_quices) {
+        this.nota_promedio_quices = nota_promedio_quices;
+    }
+
+    /**
+     * @return the nota_promedio_tareas
+     */
+    public int getNota_promedio_tareas() {
+        return nota_promedio_tareas;
+    }
+
+    /**
+     * @param nota_promedio_tareas the nota_promedio_tareas to set
+     */
+    public void setNota_promedio_tareas(int nota_promedio_tareas) {
+        this.nota_promedio_tareas = nota_promedio_tareas;
+    }
+
+    /**
+     * @return the nota_Proyecto1
+     */
+    public int getNota_Proyecto1() {
+        return nota_Proyecto1;
+    }
+
+    /**
+     * @param nota_Proyecto1 the nota_Proyecto1 to set
+     */
+    public void setNota_Proyecto1(int nota_Proyecto1) {
+        this.nota_Proyecto1 = nota_Proyecto1;
+    }
+
+    /**
+     * @return the nota_Proyecto2
+     */
+    public int getNota_Proyecto2() {
+        return nota_Proyecto2;
+    }
+
+    /**
+     * @param nota_Proyecto2 the nota_Proyecto2 to set
+     */
+    public void setNota_Proyecto2(int nota_Proyecto2) {
+        this.nota_Proyecto2 = nota_Proyecto2;
+    }
+
+    /**
+     * @return the nota_Proyecto3
+     */
+    public int getNota_Proyecto3() {
+        return nota_Proyecto3;
+    }
+
+    /**
+     * @param nota_Proyecto3 the nota_Proyecto3 to set
+     */
+    public void setNota_Proyecto3(int nota_Proyecto3) {
+        this.nota_Proyecto3 = nota_Proyecto3;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the nota_promedio_examen_quiz_tarea
+     */
+    public int getNota_promedio_examen_quiz_tarea() {
+        return nota_promedio_examen_quiz_tarea;
+    }
+
+    /**
+     * @param nota_promedio_examen_quiz_tarea the nota_promedio_examen_quiz_tarea to set
+     */
+    public void setNota_promedio_examen_quiz_tarea(int nota_promedio_examen_quiz_tarea) {
+        this.nota_promedio_examen_quiz_tarea = nota_promedio_examen_quiz_tarea;
+    }
+
+    /**
+     * @return the nota_promedio_proyectos
+     */
+    public int getNota_promedio_proyectos() {
+        return nota_promedio_proyectos;
+    }
+
+    /**
+     * @param nota_promedio_proyectos the nota_promedio_proyectos to set
+     */
+    public void setNota_promedio_proyectos(int nota_promedio_proyectos) {
+        this.nota_promedio_proyectos = nota_promedio_proyectos;
+    }
+
+    /**
+     * @return the nota_final
+     */
+    public int getNota_final() {
+        return nota_final;
+    }
+
+    /**
+     * @param nota_final the nota_final to set
+     */
+    public void setNota_final(int nota_final) {
+        this.nota_final = nota_final;
+    }
+    
+}
